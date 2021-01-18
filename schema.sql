aea555794bf52d3a36effef8e03a7a992c648e94
tlin41390 <56329045+tlin41390@users.noreply.github.com>
2021-01-18 13:24:21 -0800

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -36,8 +36,8 @@ FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 );
 
 CREATE TABLE dept_emp(
-	dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
@@ -47,14 +47,45 @@ FOREIGN KEY(dept_no) REFERENCES departments(dept_no),
 
 CREATE TABLE titles(
 	emp_no INT NOT NULL,
-	title INT NOT NULL,
+	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
-	PRIMARY KEY(emp_no,title,from_date)
+FOREIGN KEY(emp_no) REFERENCES salaries(emp_no),
+	PRIMARY KEY(emp_no,title,to_date)
 );
 
 SELECT* FROM departments;
+SELECT* FROM employees;
+Select* FROM dept_manager;
+Select* FROM dept_emp;
+Select* FROM salaries;
+Select* FROM titles;
 
-	
+SELECT  first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31'
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31'
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31'
+
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01'AND'1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
