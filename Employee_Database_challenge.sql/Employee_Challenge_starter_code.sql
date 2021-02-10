1c7a7388fd9c209f9225e6e469751e7f1adf6787
unknown <tlin41@dons.usfca.edu>
2021-02-09 16:40:54 -0800

    organized files.

--- Employee_Database_challenge.sql/Employee_Challenge_starter_code.sql
+++ Employee_Database_challenge.sql/Employee_Challenge_starter_code.sql
@@ -1,9 +1,241 @@
--- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
+--Create Table for PH Employees
+CREATE TABLE departments(
+	dept_no VARCHAR(4) NOT NULL,
+	dept_name VARCHAR(40) NOT NULL,
+	PRIMARY KEY (dept_no),
+	UNIQUE (dept_name)
+);
 
-INTO nameyourtable
-FROM _______
-ORDER BY _____, _____ DESC;
+CREATE TABLE employees(
+	emp_no INT NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	gender VARCHAR NOT NULL,
+	hire_date DATE NOT NULL,
+	PRIMARY KEY(emp_no)
+);
+
+CREATE TABLE dept_manager(
+dept_no VARCHAR(4) NOT NULL,
+	emp_no INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY(emp_no,dept_no)
+);
+
+CREATE TABLE salaries(
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE dept_emp(
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
+FOREIGN KEY(dept_no) REFERENCES departments(dept_no),
+	PRIMARY KEY(emp_no,dept_no)
+);
+
+CREATE TABLE titles(
+	emp_no INT NOT NULL,
+	title VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
+FOREIGN KEY(emp_no) REFERENCES salaries(emp_no),
+	PRIMARY KEY(emp_no,title,to_date)
+);
+
+SELECT* FROM departments;
+SELECT* FROM employees;
+Select* FROM dept_manager;
+ 
+Select* FROM salaries;
+Select* FROM titles;
+SELECT* FROM dept_emp;
+
+SELECT  first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31'
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31'
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31'
+
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT emp_no,
+	first_name, 
+last_name,
+	gender
+INTO emp_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01'AND'1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT emp_no,
+	first_name,
+last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info;
+
+SELECT d.dept_name,
+	dm.emp_no,
+	dm.from_date,
+	dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no=dm.dept_no;
+
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.to_date
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no;
+
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no=de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+SELECT*FROM salaries
+ORDER BY to_date DESC;
+
+SELECT e.emp_no,
+	e.first_name,
+e.last_name,
+	e.gender,
+	s.salary,
+	de.to_date
+	
+INTO emp_info
+From employees as e
+INNER JOIN salaries as s
+ON (e.emp_no=s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	AND (de.to_date='9999-01-01');
+	
+SELECT dm.dept_no,
+		d.dept_name,
+		dm.emp_no,
+		ce.last_name,
+		ce.first_name,
+		dm.from_date,
+		dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+	INNER JOIN departments AS d
+		ON (dm.dept_no=d.dept_no)
+	INNER JOIN current_emp AS ce
+		ON (dm.emp_no=ce.emp_no);
+
+SELECT ce.emp_no,
+		ce.first_name,
+		ce.last_name,
+		d.dept_name
+INTO dept_info
+FROM current_emp as ce
+	INNER JOIN dept_emp AS de
+		ON (ce.emp_no=de.emp_no)
+	INNER JOIN departments AS d
+		ON (de.dept_no=d.dept_no);
+
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.dept_no
+INTO retired_sales_info
+FROM retirement_info as ri
+	INNER JOIN dept_emp as de
+		ON(ri.emp_no = de.emp_no)
+		WHERE(dept_no = 'd007');
+		
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.dept_no
+INTO sales_development_info
+FROM retirement_info as ri
+INNER JOIN dept_emp as de
+	ON(ri.emp_no = de.emp_no)
+	WHERE dept_no IN('d005','d007');
+	
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+	ON(e.emp_no=t.emp_no)
+	WHERE (e.birth_date BETWEEN '01-01-1952' AND '12-31-1955')
+	ORDER BY emp_no;
+	
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no)rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title,
+rt.from_date,
+rt.to_date
+INTO unique_titles
+FROM retirement_titles as rt
+ORDER BY emp_no,to_date DESC;
+
+SELECT COUNT (ut.title), ut.title
+INTO retiring_titles
+From unique_titles as ut
+GROUP BY ut.title;
+
+
+
+
+
+	
+	
+
+
